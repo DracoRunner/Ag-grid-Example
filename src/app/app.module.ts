589f90fd8cd9d16f6e421a22c8658f1334e97e6d
DracoRunner <mahibaghel@live.co.uk>
2022-09-28 14:33:08 +0800

    6 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app.routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
